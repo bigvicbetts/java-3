f89a2fd26e50e2c344c61dc943c931c9d3c4c283
Victor Betts <v_betts22@hotmail.com>
2020-06-29 21:41:40 -0500

    Completed all bonus (I think) on Module 8 Exercise 1

--- src/concurrency/Sync.java
+++ src/concurrency/Sync.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 class Sync extends Thread {
-    List nums = new ArrayList<>();
+    static List nums = new ArrayList<>();
 
     Sync(String name) {
         super(name);
@@ -15,18 +15,36 @@ class Sync extends Thread {
     public void run() {
         Random rand = new Random();
 
+        synchronized (this) {
         for(int i=0; i<100; i++) {
             int randInt = rand.nextInt(100);
             this.nums.add(randInt);
+            System.out.println(this.getName() + " - " + randInt);
+        }
         }
     }
 
     public static void main(String[] args)  {
         Sync syncThread = new Sync("sync thread");
-        syncThread.start();
+        Sync syncThread2 = new Sync("sync thread 2");
+        Sync syncThread3 = new Sync("sync thread 3");
+        Sync syncThread4 = new Sync("sync thread 4");
+        Sync syncThread5 = new Sync("sync thread 5");
+
+        synchronized (nums) {
+            syncThread.start();
+            syncThread2.start();
+            syncThread3.start();
+            syncThread4.start();
+            syncThread5.start();
+        }
 
         try {
             syncThread.join();
+            syncThread2.join();
+            syncThread3.join();
+            syncThread4.join();
+            syncThread5.join();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
